a9c3043285fbcf53183fc8951601bfa5d0ea8126
bruno.oliveira <brunotco4@gmail.com>
2023-06-14 14:34:58 +0100

    db context and models

---
+++ ExcelDataImporterDatabase/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using ExcelDataImporterDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExcelDataImporterDatabase;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<User> User { get; set; }
+    public DatabaseContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options
+            .UseCamelCaseNamingConvention();
+    }
+}
---
+++ ExcelDataImporterDatabase/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExcelDataImporterDatabase.Models;
+
+public class User
+{
+    [Key]
+    public int? Id { get; set; }
+    public string? Fullname { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+    public bool? Active { get; set; } = true;
+}
