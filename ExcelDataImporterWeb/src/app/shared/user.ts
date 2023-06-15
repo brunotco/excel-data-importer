d2efe8c16d075540a57da34948aee9088dd8d827
bruno.oliveira <brunotco4@gmail.com>
2023-06-15 08:41:38 +0100

    user definition

---
+++ ExcelDataImporterWeb/src/app/shared/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    id: number = 0;
+    fullname: string | null = null;
+    username: string | null = null;
+    password: string | null = null;
+    email: string | null = null;
+    active: boolean | null = null;
+}
