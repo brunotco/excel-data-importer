ea987d7e6fb2d728f3b98cbde5a7dcb6646decd1
bruno.oliveira <brunotco4@gmail.com>
2023-06-15 14:48:15 +0100

    defined user class

--- ExcelDataImporterWeb/src/app/shared/user.ts
+++ ExcelDataImporterWeb/src/app/shared/user.ts
@@ -5,4 +5,12 @@ export class User {
     password: string | null = null;
     email: string | null = null;
     active: boolean | null = null;
+
+    constructor(fullname: string = '', username: string = '', password: string = '', email: string = '', active = true) {
+        this.fullname = String(fullname);
+        this.username = String(username);
+        this.password = String(password);
+        this.email = String(email);
+        this.active = active;
+    }
 }
