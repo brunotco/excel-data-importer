432128cd605cf1064b1d46dd88f14e2cfa28ca25
bruno.oliveira <brunotco4@gmail.com>
2023-06-15 08:42:04 +0100

    component for report

---
+++ ExcelDataImporterWeb/src/app/report/report.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user';
+import { ApiService } from '../services/api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { ExcelService } from '../services/excel.service';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.css']
+})
+export class ReportComponent implements OnInit {
+
+  public userData: User[] = [];
+  public columnHeaders: string[] = Object.keys(new User());
+  public tableData = new MatTableDataSource<(User)>(this.userData);
+
+  constructor(
+    private apiService: ApiService,
+    private excelService: ExcelService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  async ngAfterViewInit() {
+    this.load();
+  }
+
+  load() {
+    this.apiService.loadUser()
+    .subscribe(result => {
+      this.userData = result;
+      this.tableData.data = result;
+    });
+  }
+
+  // async waitLoad() {
+  //   return await this.apiService.loadUser().toPromise();
+  // }
+
+  mark(id: number, completed: boolean) {
+    console.log(id);
+    if (!completed) {
+      this.apiService.markActiveUser(id)
+      .subscribe(() => {
+        this.load();
+      });
+    }
+    else if (completed) {
+      this.apiService.markInactiveUser(id)
+      .subscribe(() => {
+        this.load();
+      });
+    }
+  }
+
+  allActive() {
+    this.userData.forEach(user => {
+      if(user.active === false)
+        this.apiService.markActiveUser(user.id)
+        .subscribe(() => {
+          this.load();
+        });
+    });
+  }
+
+  allInactive() {
+    this.userData.forEach(user => {
+      if(user.active === true)
+        this.apiService.markInactiveUser(user.id)
+        .subscribe(() => {
+          this.load();
+        });
+    });
+  }
+
+  export() {
+    this.excelService.writeFile(this.userData);
+  }
+
+}
